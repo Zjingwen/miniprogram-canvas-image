21219f0153bf5f9c64a2ab39266530c3c4f891f2
静文（jingwen） <jingwen@xiaoshijie.com>
2021-11-08 18:14:06 +0800

    release: 改造项目适合lib方式开发

--- src/index.js
+++ src/index.js
@@ -1,24 +1,3 @@
-const _ = require('./utils')
-
-Component({
-  properties: {
-    prop: {
-      type: String,
-      value: 'index.properties'
-    },
-  },
-  data: {
-    flag: false,
-  },
-  lifetimes: {
-    attached() {
-      wx.getSystemInfo({
-        success: () => {
-          this.setData({
-            flag: _.getFlag(),
-          })
-        }
-      })
-    }
-  }
-})
+export default function () {
+  console.log('core')
+}
--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -1,1 +1,8 @@
-Page({})
+import core from "../../components/index"
+
+Page({
+  onLoad: function (options) {
+    console.log(options);
+    core();
+  }
+})
